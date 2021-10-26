ac59427b145f199c62e151a02b346d0b3c9a8ed2
Pedrohvn <pedrohvn4@gmail.com>
2021-10-26 14:05:12 -0300

    alulas js comentario e string

---
+++ js/comentarios.js
@@ -0,0 +1,7 @@
+console.log("Bem vindo ao js.")
+
+//Barra dupla para comentar em linha console.log("Bem vindo ao js.")
+
+/*Barra asterisco para comentar
+em bloco até chegar emm um asterisco barra */
+
---
+++ js/string.js
@@ -0,0 +1,17 @@
+/* String serve para demonstrar leitura
+
+Os caracteres para string são:
+`` template literals ou template strings
+"" aspas duplas
+'' aspas simples 
+*/
+
+console.log('Apenas um exemplo')
+console.log("Ficar atento com o que for escrever para não entrar em conflito com as aspas") 
+//exemplo: 
+//console.log('Você quer um pouco d'agua') 
+
+console.log(`A crase é o melhor, por não entrar em conflito e pode 
+ser usada 
+em 
+multilinhas`)
