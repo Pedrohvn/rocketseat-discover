5639165a53f2e91eb44905c9f202d08859b8e151
Pedrohvn <pedrohvn4@gmail.com>
2021-10-26 20:52:47 -0300

    object-and-array

---
+++ js/object.js
@@ -0,0 +1,23 @@
+//object
+const person = {
+    name: 'Pedro',
+    age: 26,
+    weight: 76,
+    isAdmin: true
+}
+
+console.log(`Nome:${person.name} idade:${person.age} Admin:${person.isAdmin}`)
+
+
+//array
+
+const animals =[
+    'gato',
+    'macaco',
+    {
+        name:'mula',
+        age:8
+    }
+]
+
+console.log(animals[2].name )
