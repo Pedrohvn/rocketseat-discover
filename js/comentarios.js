ce28ea7eb9d9e225eb3b2f58d937d9961ba1387f
Pedrohvn <pedrohvn4@gmail.com>
2021-10-26 20:59:40 -0300

    aulas-comentario-e-string

--- js/comentarios.js
+++ js/comentarios.js
@@ -3,5 +3,5 @@ console.log("Bem vindo ao js.")
 //Barra dupla para comentar em linha console.log("Bem vindo ao js.")
 
 /*Barra asterisco para comentar
-em bloco até chegar emm um asterisco barra */
+em bloco até chegar em um asterisco barra */
 
