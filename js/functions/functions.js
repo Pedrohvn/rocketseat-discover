2ff44fc63ffa8b0285f2c51e60124c8240ff82a9
Pedrohvn <pedrohvn4@gmail.com>
2021-10-27 14:31:00 -0300

    function_expression

---
+++ js/functions/functions.js
@@ -0,0 +1,28 @@
+//criar um aplicativo de frases motivacionais
+
+//criando uma função
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciencia e persistencia')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+//executando a função
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+
+console.log('fim do programa')
+
+
+
+//function expression 
+
+//parêmetros (parameters)
+const sum = function(numeber1,number2){
+    console.log(numeber1 + number2)
+}
+
+sum(10,8)
