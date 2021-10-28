85b7a96455a2c0d563e1267f2b5503376c099de6
Pedrohvn <pedrohvn4@gmail.com>
2021-10-28 14:11:56 -0300

    Scope_and_return

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -30,7 +30,10 @@ sum(10,8)
 //function return
 
 const sum = function(number1,number2){
-    console.log(number1 + number2)
+    let total = (number1 + number2)
+    return total
 }
 
-let number1
+let number1 = 50
+let number2 = 13
+console.log( `A soma dos números é: ${sum(number1,number2)}`)
